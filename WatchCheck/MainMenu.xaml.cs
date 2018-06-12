e44cddca14c489bbe6342a339ea656b38c5d27b2
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-06-12 11:36:40 +0200

    nuaudio reference + commands

--- WatchCheck/MainMenu.xaml.cs
+++ WatchCheck/MainMenu.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WatchCheck
 {
@@ -23,6 +24,38 @@ namespace WatchCheck
         public MainMenu()
         {
             InitializeComponent();
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //OpenFileDialog open = new OpenFileDialog();
+            //open.Filter = "Wave File (*.wav)|*.wav";
+            MessageBox.Show("open");
+        }
+
+        private void New_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("adsfdasfasdf");
+        }
+
+        private void Exit_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Exit_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
