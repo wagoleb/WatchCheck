bd9d366d77706a04d9da237fc09f974e9855ff2e
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-06-14 14:16:50 +0200

    wave form interface

---
+++ WatchCheck/WaveFormI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFSoundVisualizationLib;
+
+namespace WatchCheck
+{
+    class WaveFormI : IWaveformPlayer
+    {
+        public double ChannelPosition { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double ChannelLength => throw new NotImplementedException();
+
+        public float[] WaveformData => throw new NotImplementedException();
+
+        public TimeSpan SelectionBegin { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public TimeSpan SelectionEnd { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsPlaying => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
